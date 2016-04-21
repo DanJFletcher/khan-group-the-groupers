1d2b713a03ae9e33bf4afa66ba3cd50207bfa2d7
Dan Fletcher <dannyjfletcher@outlook.com>
2016-04-21 15:35:32 +0000

    add new steps  scaffolding

--- validation.js
+++ validation.js
@@ -1,6 +1,9 @@
+
+// --------------------------------------
+// step 1
 staticTest($._("Add spans"), function() {
     var result = null;
-    var descrip = $._("This webpage describes groupers, a type of large ocean fish. In the first step, your job is to to make the first sentences of each paragraph stand out. Add `<*span*>` tags around each first sentence, give them a class name of 'first-sentence' and then style that class to make the text either bold or underlined.");
+    var descrip = $._("This webpage describes groupers, a type of large ocean fish. In the first step, your job is to to make the first sentences of each paragraph stand out. Start by adding `<span>` tags around each first sentence. Then give both of your `<span`> tags a class called \"first-sentence\".");
     var displayP = "";
     
     var added1SpanP = {"p span:not(:empty)": 1};
@@ -35,9 +38,23 @@ staticTest($._("Add spans"), function() {
     assertMatch(result, descrip, displayP);
 });
 
+// --------------------------------------
+// step 2
+staticTest($._("Style Spans"), function() {
+    var result = null;
+    var descrip = $._("Good! Now, using a class selector, write a CSS rule to style your \"first-sentence\" class. Make the sentence either bold, or underlined.");
+    var displayP = "";
+    
+    result = fail();
+    
+    assertMatch(result, descrip, displayP);
+});
+
+// --------------------------------------
+// step 3
 staticTest($._("Add a div"), function() {
     var result = null;
-    var descrip = $._("Now wrap all of the paragraphs in a single `<*div*>`, give it a class of 'info' and style it to have a background color.");
+    var descrip = $._("Great! Wrap the paragraphs in a single `<div>`, and then give the `div` a class of \"info\".");
     var displayP = "";
     
     var addedDivP = {"div p:not(:empty)": 1};
@@ -57,6 +74,18 @@ staticTest($._("Add a div"), function() {
             result = fail($._("Once you've added the <div>, add a style for the 'info' class that gives it a background color."));
         }
     }
+    assertMatch(result, descrip, displayP);
+});
+
+// --------------------------------------
+// step 4
+staticTest($._("Style the Div"), function() {
+    var result = null;
+    var descrip = $._("Last step! Using a class selector, write a CSS rule to style your 'info' class. Give it a cool looking background color.");
+    var displayP = "";
+    
+    result = fail();
+    
     assertMatch(result, descrip, displayP);
 });
 
